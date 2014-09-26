2f874fa69068431e3e62bb82810c8cd8075cc8c8
LegolasTheElf <pietro@suffritti.it>
2014-09-26 19:31:26 +0200

    Create mannaggia.sh
    version 0.2 of the mannaggia.sh script.

---
+++ mannaggia.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+############################################################
+# Mannaggiatore automatico per VUA depressi
+# idea originale by Alexiobash dallo script incazzatore.sh
+# ampliata, riscritta e mantenuta da Pietro "Legolas" Suffritti
+# convertita in mannaggia.sh rel 0.2
+# patcher e contributors:
+# Marco Placidi, Maurizio "Tannoiser" Lemmo
+# thanks to : Veteran Unix Admins group on Facebook
+# released under GNU-GPLv3
+############################################################
+# parametri da command line:
+# --audio : attiva mplayer per fargli pronunciare i santi
+# --spm <n> : numero di santi per minuto
+# --wall : invia l'output a tutte le console : attenzione , se non siete root o sudoers disattivare il flag -n
+
+audioflag=false
+spm=60
+spmflag=false
+wallflag=false
+DELSTRING1="</FONT>"
+DELSTRING2="</b>"
+
+# lettura parametri da riga comando
+for parm in $* 
+	do
+	# leggi dai parametri se c'e' l'audio
+	if [ $parm == "--audio" ] 
+		then 
+		audioflag=true
+	fi
+
+	# leggi dai parametri se c'e' da mandare i commenti su wall
+	if [ $parm == "--wall" ] 
+		then 
+		wallflag=true
+	fi
+
+	# se flag
+	# imposta i santi per minuto e resetta il flag
+	if [ $spmflag == true ]
+		then
+		let "spm = 60 / $parm"
+		spmflag=false
+	fi
+
+	# se parm = --spm
+	# setta un flag
+	if [ $parm == "--spm" ]
+		then
+		spmflag=true
+	fi
+done
+
+while [ 1 ] 
+	do
+	MANNAGGIA="Mannaggia `curl -s www.santiebeati.it/$(</dev/urandom tr -dc A-Z|head -c1)/|grep tit|cut -d'>' -f 4-9|shuf -n1 |awk -F "$DELSTRING1" '{print$1$2}'|awk -F "$DELSTRING2" '{print$1}'`"
+	MANNAGGIAURL="http://translate.google.com/translate_tts?tl=it&q=$MANNAGGIA"
+	
+	if [ $wallflag == true ]
+		then
+			# attenzione: se non siete root o sudoers dovete togliere dalla riga successiva "sudo" e "-n"
+			echo $MANNAGGIA | sudo wall -n
+		else
+			echo $MANNAGGIA 
+	fi
+
+	if [ $audioflag == true ]
+		then
+		mplayer -really-quiet -ao alsa "$MANNAGGIAURL" 2>/dev/null
+	fi
+
+	sleep $spm
+done
