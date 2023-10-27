7a4c64a74e858bde506e9b8ded344cb8a0106891
Matteo Croce <matteo@teknoraver.net>
2023-10-27 03:52:12 +0200

    Use HTTPS scheme
    santiebeati.it switched to HTTPS only, so curl gets a redirect instead
    of the response:
    ```
    $ curl -I http://www.santiebeati.it/
    HTTP/1.1 301 Moved Permanently
    Date: Thu, 26 Oct 2023 15:02:44 GMT
    Location: https://www.santiebeati.it/
    ```
    Rather than adding `-L` to curl to follow redirects, use the correct URL
    scheme directly.

--- mannaggia.sh
+++ mannaggia.sh
@@ -126,7 +126,7 @@ fi
 while [ "$nds" != 0 ]
 	do
 	# shellcheck disable=SC2019
-	MANNAGGIA="Mannaggia $(curl -s "www.santiebeati.it/$(</dev/urandom tr -dc A-Z|head -c1)/"|grep -a tit|cut -d'>' -f 4-9| sed '/^.$/d' | $shufCmd -n1 |awk -F "$DELSTRING1" '{print$1$2}'|awk -F "$DELSTRING2" '{print$1}' | iconv -f ISO-8859-1)"
+	MANNAGGIA="Mannaggia $(curl -s "https://www.santiebeati.it/$(</dev/urandom tr -dc A-Z|head -c1)/"|grep -a tit|cut -d'>' -f 4-9| sed '/^.$/d' | $shufCmd -n1 |awk -F "$DELSTRING1" '{print$1$2}'|awk -F "$DELSTRING2" '{print$1}' | iconv -f ISO-8859-1)"
 	if [ "$wallflag" = true ]
 		then
 		pot=$(( nds % 50 ))
